be870dae79852d91b23ee7659e1b56da8a683197
ttottotto111 <ksw4997@naver.com>
2025-03-15 15:14:18 +0900

    update : automaticlly add default settings

--- quiz/quiz_board/apps.py
+++ quiz/quiz_board/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_item(sender, **kwargs):
+    from .models import BoardSetting
+    if not BoardSetting.objects.exists():
+        BoardSetting.objects.create()
 
 class QuizBoardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'quiz_board'
+    
+    def ready(self):
+        post_migrate.connect(create_default_item, sender=self)
--- quiz/quiz_board/models.py
+++ quiz/quiz_board/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class BoardSetting(models.Model):
-    setting = models.CharField(primary_key=True, max_length=100)
-    value = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    quiz_paging = models.IntegerField(default=10)
+    question_paging = models.IntegerField(default = 10)
+    question_random = models.BooleanField(default=False)
+    choice_random = models.BooleanField(default = False)
