8487de0e3c5448f9618a8c8347ce465bdc4ddb30
ttottotto111 <ksw4997@naver.com>
2025-03-15 14:52:42 +0900

    create : add db quiz_board setting

--- quiz/quiz_board/models.py
+++ quiz/quiz_board/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class BoardSetting(models.Model):
+    setting = models.CharField(primary_key=True, max_length=100)
+    value = models.IntegerField()
