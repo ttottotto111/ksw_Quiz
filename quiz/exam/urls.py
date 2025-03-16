186e0a1f9afc32121b9fd4a346e65afb5b4a09ad
ttottotto111 <ksw4997@naver.com>
2025-03-16 17:38:51 +0900

    update : add exam start/page/sumit

--- quiz/exam/urls.py
+++ quiz/exam/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import Exam
+
+router = DefaultRouter()
+
+router.register('', Exam, basename='exam')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('quiz/', include('quiz_manager.urls')),
-    path('board/', include('quiz_board.urls')),
-    path('exam/', include('exam.urls')),
+    path('', include(router.urls)),
 ]
--- quiz/exam/views.py
+++ quiz/exam/views.py
@@ -1,3 +1,123 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+import random
+import itertools
+import copy
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from quiz_board.serializers import QuestionsSerializers
+
+from quiz_board import utils
+from quiz_manager.models import Questions, Quiz
+from .models import History
 
 # Create your views here.
+class Exam(viewsets.ViewSet):
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_session_key(self, user_id, quiz_id):
+        return f"exam_{user_id}_{quiz_id}"
+    
+    # 시험 생성
+    @action(detail = True, methods = ['post'], url_path='exam-start')
+    def exam_start(self, request, pk):
+        session_key = self.get_session_key(request.user, pk)
+        if session_key in request.session:
+            return Response(request.session[session_key])
+        
+        quiz = Quiz.objects.get(id = pk)
+        questions = list(Questions.objects.filter(quiz = pk))
+        setting = utils.get_board_setting()
+
+        # 질문 개수가 많을경우
+        if len(questions) >= quiz.questions_count:
+            if setting.question_random:
+                question_list = random.sample(questions, quiz.questions_count)
+            else:
+                question_list = questions[:quiz.questions_count]
+        else:
+            #부족한 개수
+            shortage = quiz.questions_count - len(questions)
+            if setting.question_random:
+                random_question = random.choices(questions, k=shortage)
+                question_list = questions + random_question
+                random.shuffle(question_list)
+            else:
+                # 랜덤이 아닐경우 순서대로 반복
+                question_list = list(itertools.islice(itertools.cycle(questions), quiz.questions_count))
+                
+        # 선택지 랜덤 설정
+        if setting.choice_random:
+            for idx, question in enumerate(question_list):
+                question_copy = copy.deepcopy(question)
+                answer = question_copy.answer[:]
+                random.shuffle(answer)
+                question_copy.answer = answer
+                question_list[idx] = question_copy
+        
+        question_serializer = QuestionsSerializers(question_list, many=True).data
+        request.session['questions'] = question_serializer
+        return Response({
+            "message": "start quiz",
+            "questions": request.session['questions']
+        })
+    
+    # 시험 페이지
+    @action(detail = True, methods = ['get'], url_path='exam-page')
+    def exam_page(self, request, page=1, pk = None):
+        setting = utils.get_board_setting()
+        quiz_questions = request.session.get('questions', [])
+
+        paginator = Paginator(quiz_questions, setting.question_paging)
+        question_page = paginator.get_page(page)
+        
+        now_page = request.GET.get('page', 1)
+        return Response({
+            "page": int(now_page),
+            "questions": question_page.object_list,
+            "total_pages": paginator.num_pages
+        })
+        
+    # 시험 결과 제출
+    @action(detail = True, methods = ['post'], url_path='exam-submit')
+    def exam_submit(self, request, pk = None):
+        quiz = Quiz.objects.get(id = pk)
+        submit_answers = request.data.get('answers', [])
+        questions = request.session.get('questions', [])
+        
+        if not submit_answers:
+            return Response({"message": "No answers submitted."})
+        if not questions:
+            return Response({"message": "Session close"})
+        
+        questions_count = len(request.session.get('questions', []))
+        score = 0
+        
+        for answer in submit_answers:
+            question_id = questions[int(answer.get('question_num'))-1]["id"]
+            user_answer = answer.get('user_answer')
+            question = Questions.objects.get(id = question_id)
+            
+            if not user_answer:
+                continue
+            if user_answer == question.correct_answer:
+                score += 1
+                
+        save_history = History.objects.create(
+            user_id = request.user,
+            quiz_id = quiz,
+            question_count = questions_count,
+            score = score
+        )
+        
+        return Response({
+            "message" : "submit success",
+            "title" : Quiz.objects.get(id = pk).title,
+            "question_count" : questions_count,
+            "score" : score
+        })
