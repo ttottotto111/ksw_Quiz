e9338e2174f10af6ae3261425c1315d9409afc57
ttottotto111 <ksw4997@naver.com>
2025-03-16 14:11:22 +0900

    update : add models exam History

--- quiz/exam/models.py
+++ quiz/exam/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from quiz_manager.models import Quiz
 
 # Create your models here.
+class History(models.Model):
+    user_id = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE)
+    quiz_id = models.ForeignKey(Quiz, related_name="quiz", on_delete=models.CASCADE)
+    question_count = models.IntegerField()
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
